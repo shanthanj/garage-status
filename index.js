39c4b0cf7a48a0a296333054d459747424f79104
shanthanj <shanthanj@hotmail.com>
2018-07-25 12:16:01 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 "use strict";
 
+var http = require("http");
+setInterval(function() {
+    console.log("pinging my app...");
+    http.get("http://garage-status.herokuapp.com");
+}, 120000); // every 2 minutes (300000)
+
 const express = require("express");
 const bodyParser = require("body-parser");
 const request = require("request");
