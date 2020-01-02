83685c6500e9c965834810cedc3a073912a5a634
shanthanj <shanthanj@gmail.com>
2020-01-02 08:06:25 -0500

    updating

--- index.js
+++ index.js
@@ -9,9 +9,10 @@ setInterval(function() {
 const express = require("express");
 const bodyParser = require("body-parser");
 const request = require("request");
-const sa = require("superagent")
+const sa = require("superagent");
 
 const restService = express();
+const authHeader = 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiIyZDkyZjI0YTU4MjQ0MzllYjI1ZGU2MTNkNTM4YmMyZCIsImlhdCI6MTU3Nzk2OTIyMiwiZXhwIjoxODkzMzI5MjIyfQ.DkqhGJD2GjLGV3jAPuWow_iRNOw-jBE5Sn05xqX3lqQ';
 
 restService.use(
   bodyParser.urlencoded({
@@ -22,7 +23,7 @@ restService.use(
 restService.use(bodyParser.json());
 
 restService.post("/OnLights", function(req, res) {
-	var onLightsUrl = 'https://pjhass.duckdns.org/api/services/switch/turn_on?api_password=pjhome2018';
+	var onLightsUrl = 'https://pjhass.duckdns.org/api/services/switch/turn_on';
 	onOffLights(onLightsUrl, req, function(response) {
 		if (response) {
 			return res.json(getJsonResp("Turning On " + req, "OnLights"));
@@ -33,7 +34,7 @@ restService.post("/OnLights", function(req, res) {
 });
 
 restService.post("/OffLights", function(req, res) {
-	var offLightsUrl = 'https://pjhass.duckdns.org/api/services/switch/turn_off?api_password=pjhome2018';
+	var offLightsUrl = 'https://pjhass.duckdns.org/api/services/switch/turn_off';
 	onOffLights(offLightsUrl, req, function(response) {
 		if (response) {
 			return res.json(getJsonResp("Turning Off " + req, "OffLights"));
@@ -45,7 +46,7 @@ restService.post("/OffLights", function(req, res) {
 
 restService.post("/changeChannel", function(req, res) {
    var requestedChannel = getRequestedChannelIFTTT(req);
-   var changeChannelUrl = 'https://pjhass.duckdns.org/api/services/script/turn_on?api_password=pjhome2018';
+   var changeChannelUrl = 'https://pjhass.duckdns.org/api/services/script/turn_on';
    changeChannel(changeChannelUrl, requestedChannel, function(response) {
           if (response) {
              return res.json(getJsonResp("Changing Channel to " + requestedChannel, "changeChannel"));
@@ -57,12 +58,12 @@ restService.post("/changeChannel", function(req, res) {
 
 restService.post("/garageStatus", function(req, res) {
   var requestedIntent = getRequestedIntent(req);
-  var statusUrl = 'https://pjhass.duckdns.org/api/states/cover.garage_door?api_password=pjhome2018';
-  var openMainGarageUrl = 'https://pjhass.duckdns.org/api/services/cover/open_cover?api_password=pjhome2018';
-  var closeMainGarageUrl = 'https://pjhass.duckdns.org/api/services/cover/close_cover?api_password=pjhome2018';
-  var changeChannelUrl = 'https://pjhass.duckdns.org/api/services/script/turn_on?api_password=pjhome2018';
-  var onLightsUrl = 'https://pjhass.duckdns.org/api/services/switch/turn_on?api_password=pjhome2018';
-  var offLightsUrl = 'https://pjhass.duckdns.org/api/services/switch/turn_off?api_password=pjhome2018';
+  var statusUrl = 'https://pjhass.duckdns.org/api/states/cover.garage_door';
+  var openMainGarageUrl = 'https://pjhass.duckdns.org/api/services/cover/open_cover';
+  var closeMainGarageUrl = 'https://pjhass.duckdns.org/api/services/cover/close_cover';
+  var changeChannelUrl = 'https://pjhass.duckdns.org/api/services/script/turn_on';
+  var onLightsUrl = 'https://pjhass.duckdns.org/api/services/switch/turn_on';
+  var offLightsUrl = 'https://pjhass.duckdns.org/api/services/switch/turn_off';
   var status;
 
  switch(requestedIntent) {
@@ -148,176 +149,19 @@ restService.post("/garageStatus", function(req, res) {
 
 });
 
-restService.post("/audio", function(req, res) {
-  var speech = "";
-  switch (req.body.result.parameters.AudioSample.toLowerCase()) {
-    //Speech Synthesis Markup Language
-    case "music one":
-      speech =
-        '<speak><audio src="https://actions.google.com/sounds/v1/cartoon/slide_whistle.ogg">did not get your audio file</audio></speak>';
-      break;
-    case "music two":
-      speech =
-        '<speak><audio clipBegin="1s" clipEnd="3s" src="https://actions.google.com/sounds/v1/cartoon/slide_whistle.ogg">did not get your audio file</audio></speak>';
-      break;
-    case "music three":
-      speech =
-        '<speak><audio repeatCount="2" soundLevel="-15db" src="https://actions.google.com/sounds/v1/cartoon/slide_whistle.ogg">did not get your audio file</audio></speak>';
-      break;
-    case "music four":
-      speech =
-        '<speak><audio speed="200%" src="https://actions.google.com/sounds/v1/cartoon/slide_whistle.ogg">did not get your audio file</audio></speak>';
-      break;
-    case "music five":
-      speech =
-        '<audio src="https://actions.google.com/sounds/v1/cartoon/slide_whistle.ogg">did not get your audio file</audio>';
-      break;
-    case "delay":
-      speech =
-        '<speak>Let me take a break for 3 seconds. <break time="3s"/> I am back again.</speak>';
-      break;
-    //https://www.w3.org/TR/speech-synthesis/#S3.2.3
-    case "cardinal":
-      speech = '<speak><say-as interpret-as="cardinal">12345</say-as></speak>';
-      break;
-    case "ordinal":
-      speech =
-        '<speak>I stood <say-as interpret-as="ordinal">10</say-as> in the class exams.</speak>';
-      break;
-    case "characters":
-      speech =
-        '<speak>Hello is spelled as <say-as interpret-as="characters">Hello</say-as></speak>';
-      break;
-    case "fraction":
-      speech =
-        '<speak>Rather than saying 24+3/4, I should say <say-as interpret-as="fraction">24+3/4</say-as></speak>';
-      break;
-    case "bleep":
-      speech =
-        '<speak>I do not want to say <say-as interpret-as="bleep">F&%$#</say-as> word</speak>';
-      break;
-    case "unit":
-      speech =
-        '<speak>This road is <say-as interpret-as="unit">50 foot</say-as> wide</speak>';
-      break;
-    case "verbatim":
-      speech =
-        '<speak>You spell HELLO as <say-as interpret-as="verbatim">hello</say-as></speak>';
-      break;
-    case "date one":
-      speech =
-        '<speak>Today is <say-as interpret-as="date" format="yyyymmdd" detail="1">2017-12-16</say-as></speak>';
-      break;
-    case "date two":
-      speech =
-        '<speak>Today is <say-as interpret-as="date" format="dm" detail="1">16-12</say-as></speak>';
-      break;
-    case "date three":
-      speech =
-        '<speak>Today is <say-as interpret-as="date" format="dmy" detail="1">16-12-2017</say-as></speak>';
-      break;
-    case "time":
-      speech =
-        '<speak>It is <say-as interpret-as="time" format="hms12">2:30pm</say-as> now</speak>';
-      break;
-    case "telephone one":
-      speech =
-        '<speak><say-as interpret-as="telephone" format="91">09012345678</say-as> </speak>';
-      break;
-    case "telephone two":
-      speech =
-        '<speak><say-as interpret-as="telephone" format="1">(781) 771-7777</say-as> </speak>';
-      break;
-    // https://www.w3.org/TR/2005/NOTE-ssml-sayas-20050526/#S3.3
-    case "alternate":
-      speech =
-        '<speak>IPL stands for <sub alias="indian premier league">IPL</sub></speak>';
-      break;
-  }
-  return res.json({
-    speech: speech,
-    displayText: speech,
-    source: "webhook-echo-sample"
-  });
-});
-
-restService.post("/video", function(req, res) {
-  return res.json({
-    speech:
-      '<speak>  <audio src="https://www.youtube.com/watch?v=VX7SSnvpj-8">did not get your MP3 audio file</audio></speak>',
-    displayText:
-      '<speak>  <audio src="https://www.youtube.com/watch?v=VX7SSnvpj-8">did not get your MP3 audio file</audio></speak>',
-    source: "webhook-echo-sample"
-  });
-});
-
-restService.post("/slack-test", function(req, res) {
-  var slack_message = {
-    text: "Details of JIRA board for Browse and Commerce",
-    attachments: [
-      {
-        title: "JIRA Board",
-        title_link: "http://www.google.com",
-        color: "#36a64f",
-
-        fields: [
-          {
-            title: "Epic Count",
-            value: "50",
-            short: "false"
-          },
-          {
-            title: "Story Count",
-            value: "40",
-            short: "false"
-          }
-        ],
-
-        thumb_url:
-          "https://stiltsoft.com/blog/wp-content/uploads/2016/01/5.jira_.png"
-      },
-      {
-        title: "Story status count",
-        title_link: "http://www.google.com",
-        color: "#f49e42",
-
-        fields: [
-          {
-            title: "Not started",
-            value: "50",
-            short: "false"
-          },
-          {
-            title: "Development",
-            value: "40",
-            short: "false"
-          },
-          {
-            title: "Development",
-            value: "40",
-            short: "false"
-          },
-          {
-            title: "Development",
-            value: "40",
-            short: "false"
-          }
-        ]
-      }
-    ]
-  };
-  return res.json({
-    speech: "speech",
-    displayText: "speech",
-    source: "webhook-echo-sample",
-    data: {
-      slack: slack_message
-    }
-  });
-});
 
 function getGarageStatus(urlToCall, callback) {
-  request(urlToCall, function(error, response, body) {
+	
+  var options = {
+    url: urlToCall,
+    method: 'GET',
+    headers: {
+        'Accept': 'application/json',
+        'Authorization': authHeader
+    }
+};
+	
+  request(options, function(error, response, body) {
     var parsedStatus = JSON.parse(body);
     return callback(parsedStatus.state);
   });
@@ -325,6 +169,7 @@ function getGarageStatus(urlToCall, callback) {
 
 function openOrCloseMainGarage(urlToCall, callback) {
   sa.post(urlToCall)
+    .set('Authorization', authHeader)
     .send('{"entity_id":"cover.garage_door"}')
     .end(function(err, resp) {
       return callback(true);
@@ -345,6 +190,7 @@ function onOffLights(urlToCall, requestedLight, callback) {
 	if (entityToChange != ""){
 		console.log("In: onOffLights()" + entityToChange + "URL: " + urlToCall);
       sa.post(urlToCall)
+       .set('Authorization', authHeader)
        .send('{"entity_id":"' + entityToChange + '"}')
        .end(function(err, resp) {
          return callback(true);
@@ -385,6 +231,7 @@ function changeChannel(urlToCall, requestedChannel, callback) {
    }
    if (entityToChange != ""){
       sa.post(urlToCall)
+       .set('Authorization', authHeader)
        .send('{"entity_id":"' + entityToChange + '"}')
        .end(function(err, resp) {
          return callback(true);
